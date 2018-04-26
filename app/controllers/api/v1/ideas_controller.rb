4b67f96e69df71342b1f266ba6c25e6839e062c0
sarbada jaiswal <sarbadajaiswal@sarbadas-Air.lan>
2018-04-26 21:12:09 +0545

    added function to create idea

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,8 +1,18 @@
 module Api::V1
   class IdeasController < ApplicationController
+    def create
+      idea = Idea.create(idea_params)
+      render json: idea
+    end
+
     def index
       @ideas = Idea.all
       render json: @ideas
     end
+
+    private
+      def idea_params
+        params.require(:idea).permit(:title, :body)
+      end
   end
 end
